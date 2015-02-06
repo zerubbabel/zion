0347708a3770dfe6e25ff4685cfdc4a52e3e3d76
zerubbabel <imate818@gmail.com>
2015-02-06 13:54:40 +0800

    resolve

---
+++ ajax/auth.php
@@ -0,0 +1,7 @@
+<?php
+	// /$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+
+	$json = array ('status'=>false);
+	echo json_encode($json);
+
+?>
--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,28 @@
 angular.module('indexApp', ['ngRoute'])
   .config(function($routeProvider) {
 
-    $routeProvider.when('/', {
+    $routeProvider.when('/page1', {
       templateUrl: 'views/page1.html',
       //controller: 'pagr1Ctrl'
     })
     .when('/page2', {
-      templateUrl: 'views/page2.html'
+      templateUrl: 'views/page2.html',
+      resolve: {
+        auth: ['$q', '$location', 
+        function($q, $location) {
+          
+          $.post('ajax/auth.php', function(data) {
+            var result=data;
+             if (result&&result.status){
+              alert(result);
+            }else{
+              //$location.path('/');
+              //$location.replace();  
+              return $q.reject();
+            }
+          });
+        }]
+      }
     });
     $routeProvider.otherwise({
       redirectTo: '/'
